21087057bbc9add83572d2adfd502b229c03f4a2
jormcginnis <tjbbgun36@gmail.com>
2025-07-30 22:09:28 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,1 @@
-
-self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated.');
-});
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
+// Service worker placeholder
